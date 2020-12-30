b5d2337d21a369af5ddc0f5492622c5d8ef2ea0e
Lon CHEN <lchen.prestataire@oui.sncf>
2020-12-30 14:51:29 +0100

    Add forgotten file

---
+++ first-app/src/ContactCard.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let userName;
+    export let imageSrc;
+    export let jobTitle;
+    export let shortDesc;
+</script>
+<style>
+	h1 {
+		color: blue;
+	}
+</style>
+
+<div>
+    <header>
+        <img src="{imageSrc}" alt=""/>
+        <div>
+            <h1>{userName}</h1>
+            <h2>{jobTitle}</h2>
+        </div>
+    </header>
+    <div>
+        <p>{shortDesc}</p>
+    </div>
+</div>
