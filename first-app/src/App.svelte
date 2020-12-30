b99c1677dfd6311db65712afdc3c08e1ef1e2d03
Lon CHEN <lchen.prestataire@oui.sncf>
2020-12-30 12:09:18 +0100

    Add sample projects

---
+++ first-app/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	let name = 'Max';
+	let age = 30;
+
+	function incrementAge() {
+		age += 1;
+	}
+
+	$: doubleAge = age * 2;
+	$: console.log(age);
+	$: if(age > 50) {
+		age = 20;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, my age is {age}, my double age is {doubleAge}!</h1>
+<button on:click="{incrementAge}">Change Age</button>
---
+++ project-setup/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	let name = 'Lon';
+	let age = 36;
+
+	function incrementAge() {
+		age++
+		if(age > 50) {
+			age = 20
+		}
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, my age is {age}!</h1>
+<button on:click="{incrementAge}">Click on me</button>
