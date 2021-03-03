c0fada2bd8c9e9c25caa9b509a90cad42514434f
Lon CHEN <lchen.prestataire@oui.sncf>
2021-03-03 10:22:12 +0100

    Add a modal example

--- project-setup/src/App.svelte
+++ project-setup/src/App.svelte
@@ -1,44 +1,45 @@
 <script>
-    import { each, prevent_default } from 'svelte/internal'
-    import MeetupGrid from './Meetups/MeetupGrid.svelte'
-    import Header from './UI/Header.svelte'
+    import { each, prevent_default } from "svelte/internal";
+    import MeetupGrid from "./Meetups/MeetupGrid.svelte";
+    import Header from "./UI/Header.svelte";
+    import Modal from "./UI/Modal.svelte";
 
-    let title = '';
-    let subtitle = '';
-    let contact = '';
-    let description = '';
-    let address ='';
+    let title = "";
+    let subtitle = "";
+    let contact = "";
+    let description = "";
+    let address = "";
 
     let meetups = [
         {
-            id:"m1",
-            title:"The first meetup",
+            id: "m1",
+            title: "The first meetup",
             subtitle: "Yeee",
-            description:"First mmeetup desc",
+            description: "First mmeetup desc",
             address: "130 avenue de la resistance. 93340. LE RAINCY",
-            contact:"email@test.com"
+            contact: "email@test.com",
         },
         {
-            id:"m2",
-            title:"The second meetup",
+            id: "m2",
+            title: "The second meetup",
             subtitle: "Yeee 2",
-            description:"Second mmeetup desc",
+            description: "Second mmeetup desc",
             address: "2 avenue de la resistance. 93340. LE RAINCY",
-            contact:"email2@test.com"
-        }
-    ]
+            contact: "email2@test.com",
+        },
+    ];
 
     function addMeetup() {
         const newMeetup = {
-            id:Math.random().toString(),
-            title:title,
+            id: Math.random().toString(),
+            title: title,
             subtitle: subtitle,
             description: description,
             address: address,
-            contact: contact
-        }
+            contact: contact,
+        };
 
-        meetups = [...meetups, newMeetup]
+        meetups = [...meetups, newMeetup];
     }
 </script>
 
@@ -51,29 +52,32 @@
 <Header />
 
 <main>
-    <form on:submit|preventDefault="{addMeetup}">
+    <form on:submit|preventDefault={addMeetup}>
         <div class="form-control">
             <label for="title">Title</label>
-            <input type="text" id="title" bind:value="{title}"/>
+            <input type="text" id="title" bind:value={title} />
         </div>
         <div class="form-control">
             <label for="subtitle">Subtitle</label>
-            <input type="text" id="subtitle" bind:value="{subtitle}"/>
+            <input type="text" id="subtitle" bind:value={subtitle} />
         </div>
         <div class="form-control">
             <label for="contact">Contact</label>
-            <input type="text" id="contact" bind:value="{contact}"/>
+            <input type="text" id="contact" bind:value={contact} />
         </div>
         <div class="form-control">
             <label for="address">Address</label>
-            <input type="text" id="address" bind:value="{address}"/>
+            <input type="text" id="address" bind:value={address} />
         </div>
         <div class="form-control">
             <label for="description">Description</label>
-            <textarea rows=3 id="description" bind:value="{description}"></textarea>
+            <textarea rows="3" id="description" bind:value={description} />
         </div>
         <button type="submit">Add meetup</button>
     </form>
     <MeetupGrid {meetups} />
 </main>
-
+<Modal>
+    <h1 slot="header">Yo ma poule!</h1>
+    <p>Autre chose?</p>
+</Modal>
